8fad6f7f3b973f190bde6840e901c8309882323a
Andrew <levanevskogo79@gmail.com>
2020-02-19 12:55:10 +0300

    add slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1500,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icon/left_arrow.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icon/right_arrow.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                 dots: true,
-//                 arrows: false
-//                 }
-//             }
-//         ]
-//     });
-// });
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1500,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/right_arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+        ]
+    });
+});
