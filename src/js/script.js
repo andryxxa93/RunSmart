d7d5998ca3fc6abe2e6a78d3eddc01b8e6378501
Andrew <levanevskogo79@gmail.com>
2020-03-03 13:17:05 +0300

    add pageup and smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -63,4 +63,68 @@ $('.button_mini').each(function(i) {
     })
 });
 
+
+function validaForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+              },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name:  {
+                required: "Введите своё имя",
+                minlength: jQuery.validator.format("Введите {0} символов")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+          }
+    });
+}
+
+    validaForms('#consultation-form');
+    validaForms('#consultation form');
+    validaForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+   
+   
+          $('form').trigger('reset');
+        });
+        return false;
+      });
+
+      $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+      });
+      $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
+new WOW().init();
+
 });
